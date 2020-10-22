8881e749137add3374847574eae38ff887376a55
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-10-22 17:01:33 +0500

    populated gmail column based on the (gmail.com)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, :on => :create
   after_validation :normalize_name, on: :create
-
+  before_save :record_gmail
   has_one :profile
   has_many :posts
   has_many :comments, as: :commentable
@@ -15,6 +15,13 @@ class User < ApplicationRecord
   # end
   def normalize_name
     self.email = email.downcase
+  end
+
+  def record_gmail
+    if self.email.end_with?("gmail.com")
+      self.gmail = true
+      # self.save
+    end
   end
 
 
