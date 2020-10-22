23cca2ae559e5364bd006a27c4a0e5c14719e779
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-10-22 12:41:45 +0500

    add validation on eamil uniquness at creation time

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, :on => :create
   has_one :profile
   has_many :posts
   has_many :comments, as: :commentable
   accepts_nested_attributes_for :profile, update_only: true, allow_destroy: true
+
+
+  # def email_uniqueness
+  #   byebug
+  #   validates :email, presence: true, email: true
+  #   validates :email, uniqueness: true
+  # end
 end
