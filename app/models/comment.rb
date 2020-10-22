1553c3d84ed7e76f07520e0813cc0d3eabc650b0
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-10-22 12:11:04 +0500

    add before_validation(if feild is blank put some default content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
+  before_validation :give_default_content
   validates :content, presence: true
+
+
+   def give_default_content
+    self.content = 'Default Comment' if content.blank?
+   end
 end
