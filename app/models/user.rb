69c40a74493a46d36c78a8eab0c4fb0b6c3edcca
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-10-22 13:39:43 +0500

    add after_validation on email(normalize in downcase) in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, :on => :create
+  after_validation :normalize_name, on: :create
   has_one :profile
   has_many :posts
   has_many :comments, as: :commentable
@@ -11,4 +12,8 @@ class User < ApplicationRecord
   #   validates :email, presence: true, email: true
   #   validates :email, uniqueness: true
   # end
+  def normalize_name
+
+      self.email = email.downcase
+  end
 end
